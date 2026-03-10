0b5d67f0308c511829b3668358f47920886d20aa
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2026-03-10 17:17:49 +0200

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const app = new Hono()
+
+
+
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({ connectionString })
+
+
+
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({ adapter })
+
+app.get('/', (c) => {
+    return c.text('Привіт, сервер на Hono + Prisma 7 працює!')
+})
+
+const port = 3001
+console.log(`Server is running on port ${port}`)
+
+serve({
+    fetch: app.fetch,
+    port
+})
