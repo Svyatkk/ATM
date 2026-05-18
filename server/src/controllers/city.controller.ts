9471580dcc659e0ec00affe3448e2cca6003780b
Максим Куц <ipz243_kma@student.ztu.edu.ua>
2026-05-18 14:01:50 +0300

    Refactor house service file naming

--- server/src/controllers/city.controller.ts
+++ server/src/controllers/city.controller.ts
@@ -1,6 +1,6 @@
 
 import { Context } from 'hono'
-import * as registerhouse from '../services/serivce.house'
+import * as registerhouse from '../services/service.house'
 import { JWTPayload } from 'hono/utils/jwt/types'
 import * as cityService from '../services/city.service';
 import { prisma } from '..';
